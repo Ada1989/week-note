9367a11228f36ed8d99f030e0a9ec71fb485d953
DawidMmM <danonim56@gmail.com>
2020-07-30 12:57:04 +0200

    update README.md

---
+++ precache-manifest.0eca8e75826c77fa06371d84bf1bc988.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "739fed4de018d67b986a73f6beaf97a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a0e354eda3bc12ec282",
+    "url": "/static/js/2.c9364fc4.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.c9364fc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f29a4555534d40a6e10",
+    "url": "/static/js/main.01ebf4c7.chunk.js"
+  },
+  {
+    "revision": "25a1378a32ec2b8b8317",
+    "url": "/static/js/runtime-main.7e72a08f.js"
+  },
+  {
+    "revision": "c720af74bc35b1f6dbabe9e58af22a57",
+    "url": "/static/media/iconmonstr-check-mark-2.c720af74.svg"
+  },
+  {
+    "revision": "aa72a69bfdd7308a604c3d2ac55d7d09",
+    "url": "/static/media/iconmonstr-pencil-9.aa72a69b.svg"
+  },
+  {
+    "revision": "6375717a86bdcd558c522aff052e27d2",
+    "url": "/static/media/iconmonstr-trash-can-1.6375717a.svg"
+  },
+  {
+    "revision": "e43ce6c85c8383cb4442972a422cf804",
+    "url": "/static/media/plus.e43ce6c8.svg"
+  }
+]);
